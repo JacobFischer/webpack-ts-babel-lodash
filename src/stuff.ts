b157707a13c5ce89c2cdb25fe2abb60d764ff101
Jacob Fischer <jacob.t.fischer@gmail.com>
2018-10-21 20:00:13 -0500

    Update lodash imports to support tree shaking

--- src/stuff.ts
+++ src/stuff.ts
@@ -1,8 +1,9 @@
-import { capitalize, flatMap, mapValues } from "lodash-es";
+import capitalize from "lodash-es/capitalize";
+import flatMap from "lodash-es/flatMap";
+import mapValues from "lodash-es/mapValues";
 
 // "this is an example file that does some logic by importing code");
 
 const stuff = ["foo", "bar", "baz", ["fish", "soup"], "cat", ["dog"]];
 
-export const mapped = mapValues(flatMap(stuff).map(capitalize), v => v);
-
+export const mapped = mapValues(flatMap(stuff), capitalize);
